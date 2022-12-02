c987a69acd3fe49be6f93b98a5c9132917d66f70
Gendiro <grabberisonfire@gmail.com>
2022-12-02 23:25:26 +0300

    Added leaderboard and some other minor changes

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from discord.ext import commands
 from tinydb import TinyDB, Query
 import datetime
 from PIL import Image, ImageDraw, ImageFont
+from cogs.help import Helper
+from table2ascii import table2ascii, PresetStyle
 
-# TODO снятие реакции при выполнении квеста
+# TODO level barrier roles: 10+ level, 20+ level, etc
 
 # Creates a bot (All intents just in case, help removed to be replaced by custom one)
 intents = discord.Intents.all()
@@ -17,30 +19,30 @@ db = TinyDB('db.json')
 TOKEN = db.search(Query().DISCORD_TOKEN != "")[0]['DISCORD_TOKEN']
 # Channels ids
 welcome_channel_id = 0
+if db.search(Query().type == "welcome_channel_id"):
+    welcome_channel_id = db.search(Query().type == "welcome_channel_id")[0]["value"]
 quest_channel_id = 0
+if db.search(Query().type == "quest_channel_id"):
+    quest_channel_id = db.search(Query().type == "quest_channel_id")[0]["value"]
 # Level -> needed exp to level up dict, starts from 0
 levels = db.search(Query().type == "levels")[0]["values"] if db.search(Query().type == "levels") else 0
-# To give new quests appropriate quest_id (last_quest_id + 1)
-last_quest_id = -1
-# The list of created quests
-quest_list = dict()
 
-# last_quest_id initialization (looks for the biggest id out there)
-for doc in db.search(Query().type == "quest"):
-    last_quest_id = max(last_quest_id, doc["id"])
 
-# quest_list initialization
-if not db.search(Query().type == "quest_list"):
-    quest_list = {"type": "quest_list"}
-    db.insert({"type": "quest_list"})
-else:
-    quest_list = db.search(Query().type == "quest_list")[0]
+# a decorator function to make command only accessible to quest keepers
+def quest_keeper_level(function):
+    async def wrapper(ctx):
+        keeper_role = discord.utils.get(ctx.guild.roles, name="Quest keeper")
+        if keeper_role in ctx.author.roles:
+            await function(ctx)
+        else:
+            await ctx.send("Извините, это команда только для квест киперов")
+    return wrapper
 
 
 # loading additional modules in separate files
 @bot.event
 async def on_ready():
-    await bot.load_extension('help')
+    await bot.add_cog(Helper(bot))
 
 
 # Simply adds empty profile with players id into db
@@ -55,7 +57,6 @@ def add_player_to_db(player_id):
         })
 
 
-# TODO check if some of them are already on database
 # triggers only when bot joins the server, imports all people in there into db (do this in add_player probably)
 @bot.event
 async def on_guild_join(guild):
@@ -63,7 +64,15 @@ async def on_guild_join(guild):
         add_player_to_db(member.id)
 
 
-# TODO do it only if the player is not already in the database
+# added it just in case someone logins in while the bot is offline
+@bot.command(name="add_everyone")
+@quest_keeper_level
+async def add_every_user(ctx):
+    for member in ctx.guild.members:
+        if member.id != bot.application_id:
+            add_player_to_db(member.id)
+
+
 # Greats new players and adds them to db
 @bot.event
 async def on_member_join(member):
@@ -73,13 +82,15 @@ async def on_member_join(member):
 
 
 # configure_bot group of commands
-@bot.group()
+@bot.group(name="configure_bot")
+@quest_keeper_level
 async def configure_bot(ctx):
     if ctx.invoked_subcommand is None:
         await ctx.send('Такой configure_bot комманды не существует ...')
 
 
 @configure_bot.command()
+@quest_keeper_level
 async def welcome_channel(ctx, welcome_channel_name):
     global welcome_channel_id
     channel = discord.utils.get(ctx.guild.channels, name=welcome_channel_name)
@@ -90,6 +101,7 @@ async def welcome_channel(ctx, welcome_channel_name):
 
 
 @configure_bot.command()
+@quest_keeper_level
 async def quest_channel(ctx, quest_channel_name):
     global quest_channel_id
     channel = discord.utils.get(ctx.guild.channels, name=quest_channel_name)
@@ -99,6 +111,7 @@ async def quest_channel(ctx, quest_channel_name):
 
 
 @configure_bot.command()
+@quest_keeper_level
 async def levels_config(ctx):
     global levels
     db.remove(Query().type == "levels")
@@ -108,9 +121,9 @@ async def levels_config(ctx):
     count = 0
     while msg != "выход":
         title, exp = " ".join(msg.split()[:-1]), int(msg.split()[-1])
-        levels["values"][count] = {
+        levels["values"][str(count)] = {
             "title": title,
-            "exp": int(exp)
+            "exp": exp
         }
         msg = (await bot.wait_for('message')).content
         count += 1
@@ -118,31 +131,9 @@ async def levels_config(ctx):
     levels = levels["values"]
 
 
-@configure_bot.command()
-async def give_keeper(ctx, id, delete_previous_roles=False):
-    keeper_role = discord.utils.get(ctx.guild.roles, name="Quest keeper")
-    if delete_previous_roles:
-        await keeper_role.delete
-    if keeper_role:
-        quest_keeper = db.search(Query().type == "quest_keeper")[0]
-        old_keeper = await ctx.guild.fetch_member(quest_keeper["keeper_id"])
-        await old_keeper.remove_roles(keeper_role)
-        new_keeper = await ctx.guild.fetch_member(id)
-        await new_keeper.add_roles(keeper_role)
-        quest_keeper["keeper_id"] = id
-        db.remove(Query().type == "quest_keeper")
-        db.insert(quest_keeper)
-    else:
-        role = await ctx.guild.create_role(name="Quest keeper")
-        member = await ctx.guild.fetch_member(id)
-        await member.add_roles(role)
-        db.insert({"type": "quest_keeper", "role_id": role.id, "keeper_id": id})
-
-
 @bot.command(name="create_quest")
+@quest_keeper_level
 async def create_quest(ctx):
-    global last_quest_id
-
     if not quest_channel_id:
         await ctx.send("Канал для квестов ещё не был выбран")
         return
@@ -177,10 +168,8 @@ async def create_quest(ctx):
         delete_time = None
     await ctx.send("Введите описание задания: ")
     description = (await bot.wait_for('message', check=check)).content
-    last_quest_id += 1
     result_quest = {
         "type": "quest",
-        "id": last_quest_id,
         "title": title,
         "reward": reward,
         "people_limit": people_limit,
@@ -188,7 +177,6 @@ async def create_quest(ctx):
         "delete_time": delete_time,
         "description": description
     }
-    db.insert(result_quest)
     await send_quest(ctx, result_quest)
 
 
@@ -196,7 +184,7 @@ async def send_quest(ctx, quest):
     if quest["delete_time"] is not None:
         delete_seconds = (
                 datetime.datetime.strptime(quest["delete_time"], "%Y-%m-%d %H:%M:%S") - datetime.datetime.strptime(
-                    quest["send_time"], "%Y-%m-%d %H:%M:%S")).total_seconds()
+                                            quest["send_time"], "%Y-%m-%d %H:%M:%S")).total_seconds()
     else:
         delete_seconds = None
         quest["delete_time"] = "Постоянное"
@@ -228,7 +216,7 @@ async def create_quest_image(ctx, quest, delete_seconds):
     img_draw = ImageDraw.Draw(img)
     img_draw.text((600, 400), quest["title"], fill=(244, 233, 205), font=my_font)
     img_draw.text((600, 850), add_eol(quest["description"], 46), fill=(244, 233, 205), spacing=10, font=my_font)
-    if quest["people_limit"] == "inf":
+    if quest["people_limit"] is None:
         img_draw.text((550, 1850), "Для всех", fill=(244, 233, 205),
                       font=ImageFont.truetype("OpenSans-Regular.ttf", 85))
     else:
@@ -246,17 +234,20 @@ async def create_quest_image(ctx, quest, delete_seconds):
         else:
             img_draw.text((2400, 1850), quest_dt.strftime("До %H:%M %d.%m"), fill=(244, 233, 205),
                           font=ImageFont.truetype("OpenSans-Regular.ttf", 85))
+    last_quest_id = 0
+    while os.path.isfile(f"quest_{last_quest_id}.png"):
+        last_quest_id += 1
     img.save(f"quest_{last_quest_id}.png")
     img = discord.File(f"quest_{last_quest_id}.png")
     message = await bot.get_channel(quest_channel_id).send(file=img, delete_after=delete_seconds)
-    db.remove(Query().type == "quest_list")
-    quest_list[str(message.id)] = last_quest_id
-    db.insert(quest_list)
+    quest["id"] = message.id
+    db.insert(quest)
     os.remove(f"quest_{last_quest_id}.png")
 
 
 # noinspection PyTypeChecker
 @bot.command(name="confirm_quest")
+@quest_keeper_level
 async def confirm_quest(ctx, player_name, *, quest_title):
     keeper_role = discord.utils.get(ctx.guild.roles, name="Quest keeper")
     if not (keeper_role in ctx.author.roles):
@@ -273,7 +264,7 @@ async def confirm_quest(ctx, player_name, *, quest_title):
         await ctx.send("Sorry, but there is no such quest")
         return
     quest_data = quests[0]
-    player_profile["current_quests"].remove(quest_data["id"])
+    # No need to remove the quest here, we do it in the on_raw_reaction_remove anyway
     levels_data = db.search(Query().type == "levels")[0]
     player_profile["exp"] += quest_data["reward"]
     if player_profile["exp"] >= levels_data["values"][str(player_profile["level"])]["exp"]:
@@ -281,47 +272,80 @@ async def confirm_quest(ctx, player_name, *, quest_title):
         player_profile["level"] += 1
     db.remove(Query().id == player.id)
     db.insert(player_profile)
+    quest_channel_temp = await bot.fetch_channel(quest_channel_id)
+    message = await quest_channel_temp.fetch_message(quest_data["id"])
+    player_member = await bot.fetch_user(player_profile["id"])
+    await message.remove_reaction("✅", player_member)
 
 
 @bot.event
 async def on_raw_reaction_add(payload):
-    if payload.emoji.name == "✅" and str(payload.message_id) in quest_list.keys():
+    if payload.user_id == bot.application_id:
+        return
+    if payload.emoji.name == "✅" and (db.search(Query().id == payload.message_id)):
         player = db.search(Query().id == payload.user_id)[0]
-        quest_data = db.search(Query().id == quest_list[str(payload.message_id)])[0]
+        quest_data = db.search(Query().id == payload.message_id)[0]
         if quest_data["people_limit"] is not None:
             channel = bot.get_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
             if quest_data["people_limit"] == 0:
                 await message.remove_reaction("✅", payload.member)
             else:
-                db.remove(Query().id == quest_list[str(payload.message_id)])
+                db.remove(Query().id == payload.message_id)
                 quest_data["people_limit"] -= 1
                 db.insert(quest_data)
                 if quest_data["people_limit"] == 0:
                     await message.add_reaction("❎")
-        player["current_quests"].append(quest_list[str(payload.message_id)])
+        player["current_quests"].append(payload.message_id)
         db.remove(Query().id == payload.user_id)
         db.insert(player)
 
 
 @bot.event
 async def on_raw_reaction_remove(payload):
-    if payload.emoji.name == "✅" and str(payload.message_id) in quest_list.keys():
+    if payload.emoji.name == "✅" and (db.search(Query().id == payload.message_id)):
         player = db.search(Query().id == payload.user_id)[0]
-        quest_data = db.search(Query().id == quest_list[str(payload.message_id)])[0]
+        quest_data = db.search(Query().id == payload.message_id)[0]
         if quest_data["people_limit"] is not None:
             channel = bot.get_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
-            db.remove(Query().id == quest_list[str(payload.message_id)])
+            db.remove(Query().id == payload.message_id)
             quest_data["people_limit"] += 1
             db.insert(quest_data)
             bot_user = await bot.fetch_user(1001949200600272896)
             await message.remove_reaction("❎", bot_user)
-        player["current_quests"].remove(quest_list[str(payload.message_id)])
+        player["current_quests"].remove(payload.message_id)
         db.remove(Query().id == payload.user_id)
         db.insert(player)
 
-# TODO something might go wrong with id check of other_name (not certain if it's possible, but still)
+
+def get_board():
+    players = db.search(Query().type == "player")
+    return sorted(players, key=lambda d: d['exp'] + sum(levels[str(k)]["exp"] for k in range(int(d['level']))))[::-1]
+
+
+def calculate_player_rank(player_id):
+    board = get_board()
+    for i in range(len(board)):
+        if board[i]["id"] == player_id:
+            return i + 1
+
+
+@bot.command(name="leaderboard")
+async def leaderboard(ctx):
+    board = get_board()
+    prepared_board = []
+    for i in range(len(board)):
+        overall_experience = board[i]['exp'] + sum(levels[str(k)]["exp"] for k in range(int(board[i]['level'])))
+        member = await ctx.guild.fetch_member(board[i]["id"])
+        if member.nick:
+            name = member.nick
+        else:
+            player = await bot.fetch_user(board[i]["id"])
+            name = player.name
+        prepared_board.append([i + 1, name, overall_experience])
+    output = table2ascii(header=["Rank", "Name", "Experience"], body=prepared_board, style=PresetStyle.thin_compact)
+    await ctx.send(f"```\n{output}\n```")
 
 
 @bot.command(name="profile")
@@ -338,18 +362,26 @@ async def profile(ctx, other_name=""):
             await ctx.send("Sorry, no such player is found")
             return
         author = db.search(Query().id == player.id)[0]
+        if author is None:
+            await ctx.send("profile of this user is not created yet")
+            return
         author_name = other_name
         author_avatar = player.avatar
     else:
         for document in db.all():
             if document["type"] == "player" and document["id"] == ctx.author.id:
                 author = document
+        if author is None:
+            add_player_to_db(ctx.author.id)
+            await profile(ctx)
+            return
         author_name = ctx.author.name
         author_avatar = ctx.author.avatar
     embed = discord.Embed()
     embed.set_thumbnail(url=author_avatar)
     embed.add_field(name="User: ", value=f"{author_name}")
-    embed.add_field(name="Current level", value=f"{levels[str(author['level'])]['title']}")
+    embed.add_field(name="Current level", value=f"{author['level']+1}: {levels[str(author['level'])]['title']}")
+    embed.add_field(name="Current rank", value=f"{calculate_player_rank(author['id'])}")
     embed.add_field(name="Current experience", value=f"{author['exp']}/{levels[str(author['level'])]['exp']}")
     s = "No active quests"
     for quest_id in author["current_quests"]:
